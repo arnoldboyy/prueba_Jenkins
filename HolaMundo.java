228e028d836fff1524492183ff7d67210ec61047
Kevin Arnold Cortés Pacheco <126813663+arnoldboyy@users.noreply.github.com>
2025-05-07 17:22:20 -0600

    Update HolaMundo.java

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,5 +1,5 @@
 public class HolaMundo {
     public static void main(String[] args) {
-        System.out.println("Holaaa, mundo desde Jenkins y GitHub!");
+        System.out.println("Holaaaa, mundo desde Jenkins y GitHub!");
     }
 }
